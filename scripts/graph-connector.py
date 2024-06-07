0f9fd22ac383c31251a2209c677828f3b9d4373c
Hunter Walden <htwalden@gmail.com>
2024-06-07 08:35:57 -0400

    added connector script

---
+++ scripts/graph-connector.py
@@ -0,0 +1,32 @@
+'''
+Script to show how to connect to and query the graph with python. 
+'''
+import os
+from dotenv import load_dotenv
+from neo4j import GraphDatabase
+
+load_dotenv()
+
+
+URI = os.getenv("NEO4J_URI")
+USERNAME = os.getenv("NEO4J_USERNAME")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+AUTH = (USERNAME, PASSWORD)
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+
+    query = '''
+        MATCH (m:mailPiece)-[r:`goes to`] -> (o:destination) 
+        WHERE o.destaddress_STATENAME = "Florida"
+        RETURN m, r, o
+        '''
+
+    records = driver.execute_query(
+        query,
+        database_="neo4j"
+    )
+
+print(records)
+
+driver.close()
