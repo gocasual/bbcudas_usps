417dd8fe6244b74f544b1186a87d2f748931212a
Hunter Walden <htwalden@gmail.com>
2024-06-07 21:43:14 -0400

    refactor graph connector

--- scripts/graph-connector.py
+++ scripts/graph-connector.py
@@ -1,5 +1,11 @@
 '''
 Script to show how to connect to and query the graph with python. 
+
+Use this as a python module and add:
+from graph-connector import query_executor
+
+To query data from neo4j simply write the query string and
+pass that to the `query_executor` function. 
 '''
 import os
 from dotenv import load_dotenv
@@ -7,26 +13,36 @@ from neo4j import GraphDatabase
 
 load_dotenv()
 
-def driver():
-    URI = os.getenv("NEO4J_URI")
-    USERNAME = os.getenv("NEO4J_USERNAME")
-    PASSWORD = os.getenv("NEO4J_PASSWORD")
-    AUTH = (USERNAME, PASSWORD)
+def driver_connect(original_function):
+    def driver_action(*args):
+        URI = os.getenv("NEO4J_URI")
+        USERNAME = os.getenv("NEO4J_USERNAME")
+        PASSWORD = os.getenv("NEO4J_PASSWORD")
+        AUTH = (USERNAME, PASSWORD)
 
-    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        driver = GraphDatabase.driver(URI, auth=AUTH)
         driver.verify_connectivity()
-        
+        result = original_function(driver)
+        driver.close()
+        return result
+    return driver_action 
+
+
+@driver_connect
+def query_executor(driver):
+    records = driver.execute_query(
+        query,
+        database_="neo4j"
+    )
+    return records
+
+
+if __name__=='__main__':
     query = '''
         MATCH (m:mailPiece)-[r:`goes to`] -> (o:destination) 
         WHERE o.destaddress_STATENAME = "Florida"
         RETURN m, r, o
         '''
+    data = query_executor(query)
+    print(data)
 
-    records = driver.execute_query(
-        query,
-        database_="neo4j"
-    )
-
-print(records)
-
-driver.close()
